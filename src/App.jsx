8e63e41de123e923d6800aafa9521ba1a8f8a6e2
Michael Töpfer <micha.toepfer@googlemail.com>
2024-05-02 16:44:22 +0200

    use local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,28 @@ import Form from "./Components/Form/Form";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+
+// import { useState } from "react";
 
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  // const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
 
   const addColor = (newColor) => {
     setColors([newColor, ...colors]);
+  };
+
+  const handleDeleteColor = (colorId) => {
+    setColors(colors.filter((color) => color.id !== colorId));
   };
 
   const handleUpdateColor = (colorId, updatedColor) => {
     setColors(
       colors.map((color) => (color.id === colorId ? updatedColor : color))
     );
-  };
-
-  const handleDeleteColor = (colorId) => {
-    const updateColors = colors.filter((color) => color.id !== colorId);
-    setColors(updateColors);
   };
 
   return (
